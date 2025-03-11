323501f5b2ae287c6b4b1906e0e9068da41cda0c
samplesonics.com <46668191+eodowd@users.noreply.github.com>
2025-03-11 17:28:54 +0000

    Update main.py
    fixed several search_term error

--- main.py
+++ main.py
@@ -10,16 +10,16 @@ def home():
     if request.method == "POST":
        search_term = request.form.get("search_term")
        if search_term[:3] == "ai|":
-           resultsCHAT = DDGS().chat(str(search_term), model='claude-3-haiku')
+           resultsCHAT = DDGS().chat(str(search_term[3:]), model='claude-3-haiku')
            return render_template("ai.html",resultsCHAT=resultsCHAT,search_term=search_term)
        if search_term[:5] == "news|":
-           resultsnews = DDGS().news(str(search_term), max_results=15)
+           resultsnews = DDGS().news(str(search_term[5:]), max_results=15)
            return render_template("news.html",resultsnews=resultsnews,search_term=search_term)
        if search_term[:6] == "image|":
-           resultsimages = DDGS().images(str(search_term), max_results=200)
+           resultsimages = DDGS().images(str(search_term[6:]), max_results=200)
            return render_template("images.html",resultsimages=resultsimages,search_term=search_term)
        if search_term[:6] == "video|":
-           resultsvid = DDGS().videos(str(search_term), max_results=10)
+           resultsvid = DDGS().videos(str(search_term[6:]), max_results=10)
            return render_template("video.html",resultsvid=resultsvid,search_term=search_term)
        if search_term[:8] == "podcast|":
            resultspod = podsearch.search(str(search_term[8:]), country="IE", limit=20)
